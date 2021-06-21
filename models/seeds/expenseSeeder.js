e8bc6254ac7f23811add6c5af98eb427201a7dc5
Kaiyu <anne550323@gmail.com>
2021-06-21 18:42:23 +0800

    feat: modify seeder date

--- models/seeds/expenseSeeder.js
+++ models/seeds/expenseSeeder.js
@@ -4,25 +4,25 @@ const db = require('../../config/mongoose')
 db.once('open', () => {
   Expense.create({
     name:'蛋餅',
-    date:'2021/06/01',
+    date:'2021-06-01',
     category: '早餐',
     amount:35
   },
   {
     name: '紅茶',
-    date: '2021/06/01',
+    date: '2021-06-01',
     category: '飲料',
     amount: 30
   },
   {
     name: '捷運',
-    date: '2021/06/01',
+    date: '2021-06-01',
     category: '交通',
     amount: 50
   },
   {
     name: '沙丘',
-    date: '2021/06/10',
+    date: '2021-06-01',
     category: '電影',
     amount: 260
   })
