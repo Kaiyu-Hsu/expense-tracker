30585ae3915c4350241455bbdb35e94aee77bd87
Kaiyu <anne550323@gmail.com>
2021-06-22 15:03:05 +0800

    feat: add categorySchema

--- models/record.js
+++ models/record.js
@@ -9,10 +9,6 @@ const expenseSchema = new Schema ({
     type: String,
     required: true
   },
-  category: {
-    type: String,
-    required: true
-  },
   amount: {
     type: Number,
     required: true
