e4dda90d69afccda434f636f60e3c33e70c12613
Kaiyu <anne550323@gmail.com>
2021-06-26 15:59:26 +0800

    feat: add select form & totalAmount on index but no function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -8,22 +8,72 @@ router.get('/', (req, res) => {
   Record.find()
     .lean()
     .then(records => {
+      let totalAmount = 0
+      let noRecord = false
+
+      if (records.length === 0) {
+        noRecord = true
+      }
+
       Category.find()
         .lean()
         .then(categories => {
+          
           records.forEach( record => {
-            categories.forEach( category => {
 
+            totalAmount += record.amount
+            
+            categories.forEach( category => {
+              
               if (record.category === category.name) {
                 record.category = category.icon
               }
-
+              
             })
           }) 
+          res.render('index', { records, categories, totalAmount, noRecord })
         })
-      res.render('index', { records })
+      
     })
     .catch(error => console.log(error))
 })
 
+router.post('/filter', (req, res) => {
+  const category = req.body.filterCategory
+  console.log( req.body.filterCategory )
+  let filterRecords = []
+  let totalAmount = 0
+  let noRecord = false
+
+  return Record.find().lean()
+    .then( records => {
+      Category.find().lean()
+        .then( categories => {
+          records.forEach( record => {
+            filterRecords = records.filter( record => {
+              record.category === category
+            })
+            filterRecords.forEach( filterRecord => {
+              totalAmount += filterRecord.amount
+
+              categories.forEach(category => {
+                if (filterRecord.category === category.name) {
+                  filterRecord.category = category.icon
+                }
+              })
+            })
+
+            if (filterRecords.length === 0 ) {
+              noRecord = true
+            }
+            
+          })
+          console.log(filterRecords, totalAmount )
+          res.render('index', { records: filterRecords, categories, totalAmount, category, noRecord })
+    })
+    .catch(error => console.log(error))
+  })
+
+  
+})
 module.exports = router
