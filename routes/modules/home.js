27a62a097cb28e6c844e30bc7b6b747d5ed7327c
Kaiyu <anne550323@gmail.com>
2021-06-30 16:42:04 +0800

    feat: modify totalAmount & icon for each category

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -18,65 +18,66 @@ router.get('/', (req, res) => {
       Category.find()
         .lean()
         .then(categories => {
-          
-          records.forEach( record => {
+
+          records.forEach(record => {
 
             totalAmount += record.amount
-            
-            categories.forEach( category => {
-              
+
+            categories.forEach(category => {
+
               if (record.category === category.name) {
                 record.category = category.icon
               }
-              
+
             })
-          }) 
+          })
           res.render('index', { records, categories, totalAmount, noRecord })
         })
-      
+
     })
     .catch(error => console.log(error))
 })
 
 router.post('/filter', (req, res) => {
   const category = req.body.filterCategory
-  console.log( req.body.filterCategory )
+  console.log(req.body.filterCategory)
   let filterRecords = []
   let totalAmount = 0
   let noRecord = false
 
   return Record.find()
     .lean()
-    .then( records => {
+    .then(records => {
       Category.find()
         .lean()
-        .then( categories => {
-          records.forEach( record => {
-            if(record.category === category ) { 
-              filterRecords.push(record) 
+        .then(categories => {
+          records.forEach(record => {
+            if (record.category === category) {
+              filterRecords.push(record)
             }
-             
-            filterRecords.forEach( filterRecord => {
-              totalAmount += filterRecord.amount
-              
-              categories.forEach(category => {
+
+            categories.forEach(category => {
+              filterRecords.forEach(filterRecord => {
                 if (filterRecord.category === category.name) {
-                  filterRecord.category = category.icon  
+                  filterRecord.category = category.icon
                 }
               })
             })
-
-            if (filterRecords.length === 0 ) {
-              noRecord = true
-            }
-            
           })
-          
+          filterRecords.forEach(filterRecord => {
+            totalAmount += filterRecord.amount
+          })
+
+          if (filterRecords.length === 0) {
+            noRecord = true
+          }
           res.render('index', { records: filterRecords, categories, totalAmount, category, noRecord })
+        })
+
     })
     .catch(error => console.log(error))
-  })
-
-  
 })
+
+
+
 module.exports = router
